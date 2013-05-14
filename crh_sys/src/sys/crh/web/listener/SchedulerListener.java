9990c784f65f658a1680283fff4186967a2650a0
lihe <lihe@lihe-Lenovo>
2013-05-14 20:31:34 +0800

    架构好调度框架，准备周四给老师看，等待具体代码
    Signed-off-by: lihe <lihe@lihe-Lenovo>

---
+++ crh_sys/src/sys/crh/web/listener/SchedulerListener.java
@@ -0,0 +1,20 @@
+package sys.crh.web.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class SchedulerListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
